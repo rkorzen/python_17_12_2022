cf330453d6800ab9047ba65ef24fdbcb847ff067
rkorzen <korzeniewski@gmail.com>
2023-01-21 11:49:14 +0100

    circle

--- exercises/circle.py
+++ exercises/circle.py
@@ -10,7 +10,10 @@ c.perimeter # 2 * Pi * 1
 c.area      # pi * 1 ** 2
 
 c.area = 5
+c.perimeter = 10
 c.radius # nowy promien
+
+c.radius = 12
 
 
 c = Cricle(-10)
@@ -18,3 +21,20 @@ ValueError
 
 
 """
+
+from math import pi
+
+class Circle:
+    def __init__(self, r=1):
+        self.r = r
+    @property
+    def area(self):
+        return self.r ** 2 * pi
+
+    @area.setter
+    def area(self, value):
+
+    @property
+    def perimeter(self):
+        return 2 * pi * self.r
+
