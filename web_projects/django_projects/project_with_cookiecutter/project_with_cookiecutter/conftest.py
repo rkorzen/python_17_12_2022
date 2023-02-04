9b88488c943446c933410b34e86e51296719aae5
rkorzen <korzeniewski@gmail.com>
2023-02-04 11:21:39 +0100

    django project - first blood

---
+++ web_projects/django_projects/project_with_cookiecutter/project_with_cookiecutter/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from project_with_cookiecutter.users.models import User
+from project_with_cookiecutter.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
---
+++ web_projects/django_projects/todos/README.md
@@ -0,0 +1,12 @@
+#
+
+## cwiczenie - routing + views
+
+1. Utwórz aplikację - calculus
+2. Utwórz widok/i obsługujące adresy
+
+```
+   /calculus/add/1/2
+   /calculus/sub/1/2 
+   ...
+```
