2ec47e66110e83c9154233441096e5fa7f32503f
rkorzen <korzeniewski@gmail.com>
2023-02-04 11:44:18 +0100

    refactoring step 1

--- web_projects/django_projects/todos/calculus/views.py
+++ web_projects/django_projects/todos/calculus/views.py
@@ -4,17 +4,29 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def add(a, b): return a + b
+
+
+def mul(a, b): return a * b
+
+
+def div(a, b): return a / b
+
+
+def sub(a, b): return a - b
+
+
+operations = {
+    "add": add,
+    "mul": mul,
+    "sub": sub,
+    "div": div
+}
+
+
 def calculator(request, op, a, b):
-    if op == "add":
-        result = a + b
-
-    elif op == "sub":
-        result = a - b
-
-    elif op == "div":
-        result = a / b
-
-    elif op == "mul":
-        result = a * b
-
+    if op in operations:
+        result = operations[op](a, b)
+    else:
+        result = "Niedozwolona operacja"
     return HttpResponse(result)
