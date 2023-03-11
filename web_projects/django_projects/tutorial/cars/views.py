708fd5cc68e7532606a5b7058373b009ad196569
rkorzen <korzeniewski@gmail.com>
2023-03-11 15:23:52 +0100

    generics in cars

--- web_projects/django_projects/tutorial/cars/views.py
+++ web_projects/django_projects/tutorial/cars/views.py
@@ -1,39 +1,19 @@
 from rest_framework import generics
-from rest_framework import mixins
 
 from cars.models import Car
 from cars.serializers import CarSerializer
 
 
 class CarList(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
-    generics.GenericAPIView,
+    generics.ListCreateAPIView
 ):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
 
 
 class CarDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView,
+    generics.RetrieveUpdateDestroyAPIView
 ):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
