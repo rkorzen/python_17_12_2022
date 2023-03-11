538c873c847e42ca5040b7909f2580bffbda4032
rkorzen <korzeniewski@gmail.com>
2023-03-11 15:17:55 +0100

    mixins in views

--- web_projects/django_projects/tutorial/README.md
+++ web_projects/django_projects/tutorial/README.md
@@ -6,3 +6,5 @@
 2. dodaj serializer oparty o model
 3. widoki klasowe oparte o APIView (widok listy, widok szczegółów)
 4. Dodac z poziomu shell ze dwa obiekty
+
+## przejśc na widoki klasowe oparte o mixins
--- web_projects/django_projects/tutorial/cars/views.py
+++ web_projects/django_projects/tutorial/cars/views.py
@@ -1,46 +1,39 @@
-from django.http import Http404
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
+from rest_framework import mixins
 
 from cars.models import Car
 from cars.serializers import CarSerializer
 
 
-class CarList(APIView):
-    def get(self, request):
-        cars = Car.objects.all()
-        serializer = CarSerializer(cars, many=True)
-        return Response(serializer.data)
+class CarList(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
 
-    def post(self, request):
-        serializer = CarSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
 
-class CarDetail(APIView):
-    def get_object(self, id):
-        try:
-            return Car.objects.get(id=id)
-        except Car.DoesNotExist:
-            raise Http404
+class CarDetail(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    generics.GenericAPIView,
+):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
 
-    def get(self, request, id):
-        serializer = CarSerializer(self.get_object(id))
-        return Response(serializer.data)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, id):
-        snippet = self.get_object(id)
-        serializer = CarSerializer(snippet, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
 
-    def delete(self, request, id):
-        snippet = self.get_object(id)
-        snippet.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
