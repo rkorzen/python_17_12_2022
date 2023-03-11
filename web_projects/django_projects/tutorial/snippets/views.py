3a95ce0a641528a2cbd31d8305a82cc681fae40e
rkorzen <korzeniewski@gmail.com>
2023-03-11 12:13:12 +0100

    widok szczegółów

--- web_projects/django_projects/tutorial/snippets/views.py
+++ web_projects/django_projects/tutorial/snippets/views.py
@@ -27,3 +27,36 @@ def snippet_list(request):
             serializer.save()
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+
+# pip install httpie
+# http
+
+
+@csrf_exempt
+def snippet_details(request, id):
+    """
+    GET /snippet/1 - szczegóły
+    PUT /snippet/1 - aktualizacja
+    DELETE /snippet/1 - usnięcie zasobu
+    """
+    try:
+        snippet = Snippet.objects.get(id=id)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == "GET":
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == "DELETE":
+        snippet.delete()
+        return HttpResponse(status=204)
